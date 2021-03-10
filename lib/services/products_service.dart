1a91ac3a0cb888cd4773e5c670acf7a9bbbb2099
Adam Deeley <adam.deeley@oneiota.co.uk>
2021-03-10 16:17:48 +0000

    Extract the toProductList method.

--- lib/services/products_service.dart
+++ lib/services/products_service.dart
@@ -6,18 +6,37 @@ class ProductsService {
 
   ProductsService(this.dio);
 
-  ///
   Future<List<dynamic>> getProducts() async {
-    final Response<dynamic> response = await dio.get(
-        'https://s3-eu-west-1.amazonaws.com/api.themeshplatform.com/products.json',
-        options: Options(responseType: ResponseType.json));
+    Response<dynamic> response;
 
-    final Map<String, dynamic> data = response.data;
-    final productList = (data as List)
-        ?.cast<Map<String, dynamic>>()
-        ?.map((productJson) => ProductModel.fromJson(productJson))
-        ?.toList(); // extract to product list method in ProductModel
+    try {
+      response = await dio.get(
+          'https://s3-eu-west-1.amazonaws.com/api.themeshplatform.com/products.json',
+          options: Options(responseType: ResponseType.json));
+    } catch (error) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx and is also not 304.
+      if (error.response) {
+        print(error.response.data);
+        print(error.response.headers);
+        print(error.response.request);
+      } else {
+        // Something happened in setting up or sending the request that triggered an Error
+        print(error.request);
+        print(error.message);
+      }
+    }
 
+    final List<dynamic> data = response.data;
+    List<ProductModel> productList = toProductList(data);
+
+    return productList;
+  }
+
+  List<ProductModel> toProductList(List<dynamic> data) {
+    final List<ProductModel> productList = data
+        .map((productJson) => ProductModel.fromJson(productJson))
+        .toList(); // extract to product list method in ProductModel
     return productList;
   }
 }
